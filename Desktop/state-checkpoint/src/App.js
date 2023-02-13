80abb5cd7c26fea84a453295da6bd772c0ee07a5
firefly1919 <114675095+firefly1919@users.noreply.github.com>
2023-02-13 13:42:57 +0100

    state

---
+++ Desktop/state-checkpoint/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Counter from "./components/Counter";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state={isShow:true}
+  }
+  ToggleVisibility=()=>{
+    this.setState({isShow:!this.state.isShow})
+  }
+  render() { 
+    console.log("rendering");
+    return ( 
+      <div>
+      <h1>Counter</h1>
+      <button onClick={this.ToggleVisibility}>Show Counter</button>
+       {this.state.isShow && <Counter/>}
+      </div>
+     );
+  }
+}
+ 
+export default App;
+
